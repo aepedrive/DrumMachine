93c1ba7f9fb0576e6ed86242827e4deee5b3dae8
aepedrive <armando.pedraza1979@gmail.com>
2014-05-01 12:25:22 -0600

    Completed GUI
    Basic structure to create a GUI skeleton, it doesn't have functionality
    yet.

--- drummachine.py
+++ drummachine.py
@@ -38,11 +38,34 @@ class DrumMachine():
             self.drum_entry = Entry(left_frame)
             self.drum_entry.grid(row=i, column=4, padx=7, pady=2)
 
+    def create_right_pad(self):
+        right_frame = Frame(self.root)
+        right_frame.grid(row=10, column=6, sticky=W+E+N+S, padx=15, pady=2)
+        self.button = [[0 for x in range(4)] for x in range(MAX_DRUM_NUM)]
+        for i in range(MAX_DRUM_NUM):
+            for j in range(4):
+                self.button[i][j] = Button(right_frame, bg='grey55')
+                self.button[i][j].grid(row=i, column=j)
+
+    def create_play_bar(self):
+        playbar_frame = Frame(self.root, height=15)
+        ln = MAX_DRUM_NUM + 10
+        playbar_frame.grid(row=ln, columnspan=13, sticky=W+E, padx=15, pady=10)
+        button = Button(playbar_frame, text='Play')
+        button.grid(row=ln, column=1, padx=1)
+        button = Button(playbar_frame, text='Stop')
+        button.grid(row=ln, column=3, padx=1)
+        loop = BooleanVar()
+        loopbutton = Checkbutton(playbar_frame, text='Loop', variable=loop)
+        loopbutton.grid(row=ln, column=16, padx=1)
+
     def app(self):
         self.root = Tk()
         self.root.title("Drum Beast")
         self.create_top_bar()
         self.create_left_pad()
+        self.create_right_pad()
+        self.create_play_bar()
         self.root.mainloop()
 
 # ================================================
