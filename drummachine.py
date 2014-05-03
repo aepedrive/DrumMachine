4757b6919ca3586c85d1ba6181bb3e09b901c506
aepedrive <armando.pedraza1979@gmail.com>
2014-05-03 07:17:51 -0600

    Added Spinbox functionality
    It creates BPU x number of units.
    Button now toggle color when clicked.

--- drummachine.py
+++ drummachine.py
@@ -17,14 +17,14 @@ class DrumMachine():
         self.units = IntVar()
         self.units.set(4)
         self.units_widget = Spinbox(topbar_frame, from_=1, to=8, width=5,
-                                    textvariable=self.units)
+                                    textvariable=self.units, command=self.create_right_pad)
         self.units_widget.grid(row=0, column=5)
         #
         Label(topbar_frame, text='BPUs:').grid(row=0, column=6)
         self.bpu = IntVar()
         self.bpu.set(4)
         self.bpu_widget = Spinbox(topbar_frame, from_=1, to=10, width=5,
-                                  textvariable=self.bpu)
+                                  textvariable=self.bpu, command=self.create_right_pad)
         self.bpu_widget.grid(row=0, column=7)
 
     def create_left_pad(self):
@@ -39,12 +39,18 @@ class DrumMachine():
             self.drum_entry.grid(row=i, column=4, padx=7, pady=2)
 
     def create_right_pad(self):
+        bpu = self.bpu.get()
+        units = self.units.get()
+        c = bpu * units
         right_frame = Frame(self.root)
         right_frame.grid(row=10, column=6, sticky=W+E+N+S, padx=15, pady=2)
-        self.button = [[0 for x in range(4)] for x in range(MAX_DRUM_NUM)]
+        self.button = [[0 for x in range(c)] for x in range(MAX_DRUM_NUM)]
         for i in range(MAX_DRUM_NUM):
-            for j in range(4):
-                self.button[i][j] = Button(right_frame, bg='grey55')
+            for j in range(c):
+                self.active = False
+                color = 'grey55' if (j/bpu)%2 else 'khaki'
+                self.button[i][j] = Button(right_frame, bg=color, width=1,
+                                           command=self.button_clicked(i, j, bpu))
                 self.button[i][j].grid(row=i, column=j)
 
     def create_play_bar(self):
@@ -58,6 +64,14 @@ class DrumMachine():
         loop = BooleanVar()
         loopbutton = Checkbutton(playbar_frame, text='Loop', variable=loop)
         loopbutton.grid(row=ln, column=16, padx=1)
+
+    def button_clicked(self, i, j, bpu):
+        def callback():
+            btn = self.button[i][j]
+            color = 'grey55' if (j/bpu)%2 else 'khaki'
+            new_color = 'green' if btn.cget('bg') != 'green' else color
+            btn.config(bg=new_color)
+        return callback
 
     def app(self):
         self.root = Tk()
